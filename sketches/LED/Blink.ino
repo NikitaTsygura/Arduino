4b2893ca7c0ca8e757970055d1105000e6793b33
NikitaTsygura <tsyguranikita@gmail.com>
2026-01-22 19:57:25 +0000

    Create Blink.ino with LED functionality

---
+++ sketches/LED/Blink.ino
@@ -0,0 +1,12 @@
+const int LED = LED_BUILTIN;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
